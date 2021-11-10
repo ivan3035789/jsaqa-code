998389ccbe321f7a2728dd07cfdd205a71e91bae
ivan3035789 <ivan3035789@gmail.com>
2021-11-10 17:30:38 +0300

    added tests

--- 7.3/jest/test/unit/sortByName.test.js
+++ 7.3/jest/test/unit/sortByName.test.js
@@ -1,7 +1,7 @@
 const sorting = require("../../app");
 
 describe("Books names test suit", () => {
-  it("Books names should be sorted in ascending order", () => {
+  it("Books names should be sorted in ascending order test_1", () => {
     expect(
       sorting.sortByName([
         "Гарри Поттер",
@@ -11,6 +11,22 @@ describe("Books names test suit", () => {
     ).toEqual([
       "Властелин Колец",
       "Волшебник изумрудного города",
+      "Гарри Поттер",
+    ]);
+  });
+
+  it("Books names should be sorted in ascending order test_2", () => {
+    expect(
+      sorting.sortByName([
+        "Гарри Поттер",
+        "Властелин Колец",
+        "Гарри Поттер",
+        "Волшебник изумрудного города",
+      ])
+    ).toEqual([
+      "Властелин Колец",
+      "Волшебник изумрудного города",
+      "Гарри Поттер",
       "Гарри Поттер",
     ]);
   });
--- 7.3/playwright/NetologyTest.js
+++ 7.3/playwright/NetologyTest.js
@@ -1,16 +1,42 @@
 const { chromium } = require("playwright");
-
-(async () => {
+const user = require("../playwright/user");
+const { test, expect } = require("@playwright/test");
+("Успешная авторизация",
+async () => {
   const browser = await chromium.launch({
     headless: false,
-    slowMo: 5000,
-    devtools: true
+    // slowMo: 5000,
+    // devtools: true,
   });
   const page = await browser.newPage();
-  await page.goto("https://netology.ru");
-  await page.click("text=Каталог курсов");
-  await page.pause();
+  await page.goto("https://netology.ru/?modal=sign_in");
+  await page.waitForTimeout(5000);
+  await page.screenshot({ path: "screenshot_1.png" });
+  await page.fill('[placeholder="Email"]', user.validEmail);
+  await page.click('[placeholder="Пароль"]');
+  await page.fill('[placeholder="Пароль"]', user.validPassword);
+  await Promise.all([
+    page.waitForNavigation(/*{ url: 'https://netology.ru/profile' }*/),
+    page.click("text=Войти"),
+  ]);
+  await page.waitForTimeout(10000);
+  await page.screenshot({ path: "screenshot_2.png" });
+  await expect("Мои курсы и профессии").toEqual(await page.textContent("h2")); // .replace(/["']+/g, "");
+  await expect(page.locator("text=Мои курсы и профессии"));
 
-  //assertion
+  "Неуспешная авторизация",
+    await page.goto("https://netology.ru/?modal=sign_in");
+  await page.waitForTimeout(5000);
+  await page.screenshot({ path: "screenshot_3.png" });
+  await page.fill('[placeholder="Email"]', user.validEmail);
+  await page.click('[placeholder="Пароль"]');
+  await page.fill('[placeholder="Пароль"]', user.invalidPassword);
+  await page.click("text=Войти");
+  await page.waitForTimeout(7000);
+  await expect(
+    page.locator("text=Вы ввели неправильно логин или пароль")
+  ).toBeVisible();
+  await page.waitForTimeout(5000);
+  await page.screenshot({ path: "screenshot_4.png" });
   await browser.close();
 })();
